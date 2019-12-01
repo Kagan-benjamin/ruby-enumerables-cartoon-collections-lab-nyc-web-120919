6d3305194036f97ebde3d6be9210ad82aa25d8df
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-12-01 18:04:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
   cheese_types.include?(array) 
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
+ 
 end
